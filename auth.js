c7f519102b79331e94cd9f4d79463411651eecda
Azizul <khanazizul1199@gmail.com>
2024-11-24 17:37:19 +0600

    waiting promise implemented

--- auth.js
+++ auth.js
@@ -1,1 +1,10 @@
 console.log("This is auth module");
+
+const wait = (time) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Waiting done");
+		}, time);
+	});
+
+wait(2000).then((res) => console.log(res));
